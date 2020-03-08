95d343b2be73f4a26f34b0bc3bb7453ec205f486
Amir Bahrawy <amirbahrawy@gmail.com>
2020-03-08 18:22:18 +0200

    use dataBinding in MainActivity

--- app/src/main/java/com/alyndroid/architecturepatternstutorialshomework/ui/MainActivity.java
+++ app/src/main/java/com/alyndroid/architecturepatternstutorialshomework/ui/MainActivity.java
@@ -6,57 +6,47 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alyndroid.architecturepatternstutorialshomework.R;
+import com.alyndroid.architecturepatternstutorialshomework.databinding.ActivityMainBinding;
 import com.alyndroid.architecturepatternstutorialshomework.db.DataBase;
 import com.alyndroid.architecturepatternstutorialshomework.pojo.NumberModel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, NumbersView {
     //MVC
-    private Button plusButton;
-    private TextView plusResultTextView;
     private NumberModel nummberModel;
     private int plusResult;
     //MVP
     private NumbersPresenter numbersPresenter;
-    private Button divButton;
-    private TextView divResultTextView;
     //MVVM
     private NumbersViewModel numbersViewModel;
-    private Button mulButton;
-    private TextView mulResultTextView;
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         initView();
     }
 
     private void initView() {
         //MVC
-        plusButton = (Button) findViewById(R.id.plus_button);
-        plusButton.setOnClickListener(this);
-        plusResultTextView = (TextView) findViewById(R.id.plus_result_textView);
+        binding.plusButton.setOnClickListener(this);
         nummberModel = new DataBase().getNumbers();
         plusResult = nummberModel.getFirstNum() + nummberModel.getSecondNum();
         //MVP
         numbersPresenter = new NumbersPresenter(this);
-        divButton = (Button) findViewById(R.id.div_button);
-        divButton.setOnClickListener(this);
-        divResultTextView = (TextView) findViewById(R.id.div_result_textView);
+        binding.divButton.setOnClickListener(this);
         //MVVM
-        mulButton = (Button) findViewById(R.id.mul_button);
-        mulButton.setOnClickListener(this);
-        mulResultTextView = (TextView) findViewById(R.id.mul_result_textView);
+        binding.mulButton.setOnClickListener(this);
         numbersViewModel = ViewModelProviders.of(this).get(NumbersViewModel.class);
         numbersViewModel.modelMutableLiveData.observe(this, new Observer<NumberModel>() {
             @Override
             public void onChanged(NumberModel numberModel) {
                 int divResult = numberModel.getFirstNum() * numberModel.getSecondNum();
-                mulResultTextView.setText(divResult+"");
+                binding.mulResultTextView.setText(divResult+"");
             }
         });
 
@@ -65,19 +55,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         //MVC
-        if (v == plusButton)
-            plusResultTextView.setText(plusResult + "");
+        if (v==binding.plusButton)
+            binding.plusResultTextView.setText(plusResult + "");
         //MVP
-        if (v.getId() == R.id.div_button)
+        if (v == binding.divButton)
             numbersPresenter.getNumbers();
         //MVVM
-        if (v==mulButton)
+        if (v==binding.mulButton)
                numbersViewModel.getNumbers();
     }
 
     @Override
     public void onGetNumbers(NumberModel numberModel) {
         int divResult = numberModel.getFirstNum() / numberModel.getSecondNum();
-        divResultTextView.setText(divResult + "");
+        binding.divResultTextView.setText(divResult + "");
     }
 }
